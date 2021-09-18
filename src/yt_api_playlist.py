89b5ca9cedf73250db9c95367ccfd7b307c4d4bf
Oytizzo <oytizzo@gmail.com>
2021-09-18 10:55:37 +0600

    Complete yt_playlist_duration

--- src/yt_api_playlist.py
+++ src/yt_api_playlist.py
@@ -1,4 +1,6 @@
 import os
+import re
+from datetime import timedelta
 from googleapiclient.discovery import build
 
 
@@ -7,18 +9,59 @@ def main():
     api_service_name = "youtube"
     api_version = "v3"
 
+    # panda playlist from corey
+    # pl_id = "PL-osiE80TeTsWmV9i9c58mdDCSskIFdDS"
+    # python playlist from corey
+    pl_id = "PL-osiE80TeTt2d9bfVyTiXJA-UTHn6WwU"
     yt = build(api_service_name, api_version, developerKey=api_key)
 
-    pl_request = yt.playlists().list(
-        part="contentDetails, snippet",
-        channelId="UCCezIgC97PvUuR4_gbFUs5g"
-    )
-    pl_response = pl_request.execute()
+    hours_pattern = re.compile(r'(\d+)H')
+    minutes_pattern = re.compile(r'(\d+)M')
+    seconds_pattern = re.compile(r'(\d+)S')
 
-    # print(pl_response)
-    for item in pl_response["items"]:
-        print(item)
-        print()
+    total_seconds = 0
+
+    nextPageToken = None
+
+    while True:
+        pl_request = yt.playlistItems().list(part="contentDetails", playlistId=pl_id, maxResults=50, pageToken=nextPageToken)
+        pl_response = pl_request.execute()
+
+        vid_ids = []
+        for item in pl_response["items"]:
+            vid_ids.append(item['contentDetails']['videoId'])
+        print(len(vid_ids))
+        
+        
+        vid_request = yt.videos().list(part="contentDetails", id=','.join(vid_ids))
+        vid_response = vid_request.execute()
+
+        for item in vid_response['items']:
+            duration = item['contentDetails']['duration']
+            
+            hours = hours_pattern.search(duration)
+            minutes = minutes_pattern.search(duration)
+            seconds = seconds_pattern.search(duration)
+
+            hours = int(hours.group(1)) if hours else 0
+            minutes = int(minutes.group(1)) if minutes else 0
+            seconds = int(seconds.group(1)) if seconds else 0
+
+            video_seconds = timedelta(hours=hours, minutes=minutes, seconds=seconds).total_seconds()
+
+            total_seconds += video_seconds
+        
+        nextPageToken = pl_response.get('nextPageToken')
+
+        if not nextPageToken:
+            break
+
+    total_seconds = int(total_seconds)
+
+    minutes, seconds = divmod(total_seconds, 60)
+    hours, minutes = divmod(minutes, 60)
+
+    print(f'{hours}:{minutes}:{seconds}')
 
 
 if __name__ == "__main__":
