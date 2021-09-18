37d35d73e85c113763359d05866d42eb808ef51d
Oytizzo <oytizzo@gmail.com>
2021-09-18 09:19:37 +0600

    Init and complete yt_api for getting the statistics from the youtube channel

--- src/yt_api.py
+++ src/yt_api.py
@@ -1,1 +1,22 @@
-print("Helllllo world!")
+import os
+from googleapiclient.discovery import build
+
+
+def main():
+    api_key = os.environ.get("yt_developer_api_key")
+    api_service_name = "youtube"
+    api_version = "v3"
+
+    yt = build(api_service_name, api_version, developerKey=api_key)
+
+    request = yt.channels().list(
+        part="statistics",
+        id="UCCezIgC97PvUuR4_gbFUs5g",
+    )
+
+    response = request.execute()
+
+    print(response)
+
+if __name__ == "__main__":
+    main()
